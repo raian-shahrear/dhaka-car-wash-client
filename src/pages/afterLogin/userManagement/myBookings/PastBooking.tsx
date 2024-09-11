7ed8653f445b00dcc0a47996689a220723602ac7
raianshahrear <raian@nextgenitltd.com>
2024-09-12 00:19:00 +0600

    user-booking added

--- src/pages/afterLogin/userManagement/myBookings/PastBooking.tsx
+++ src/pages/afterLogin/userManagement/myBookings/PastBooking.tsx
@@ -13,7 +13,64 @@ const PastBooking = () => {
       <div className="flex items-center justify-between flex-col sm:flex-row gap-2 mb-10">
         <h1 className="text-lg sm:text-xl font-bold">Past Bookings</h1>
       </div>
-      
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-12">SL</TableHead>
+            <TableHead className="w-52">Service</TableHead>
+            <TableHead>Booked Date</TableHead>
+            <TableHead>Slot</TableHead>
+            <TableHead>Service Cost</TableHead>
+            <TableHead>Transaction Id</TableHead>
+            <TableHead className="w-52">Vehicle Details</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell className="font-bold">Car Disinfecting</TableCell>
+            <TableCell className="font-medium">2024-07-31</TableCell>
+            <TableCell className="font-medium">
+              <div className="flex flex-col gap-1 min-w-20">
+                <div>
+                  <span>Start : </span>
+                  <span>12:00</span>
+                </div>
+                <div>
+                  <span>End : </span>
+                  <span>13:00</span>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell className="font-medium">$50</TableCell>
+            <TableCell className="font-medium">Tx2345780765gg544</TableCell>
+            <TableCell className="font-medium">
+              <div className="flex flex-col gap-1">
+                <div className="grid grid-cols-[100px_auto]">
+                  <span>Type :</span>
+                  <span>Car</span>
+                </div>
+                <div className="grid grid-cols-[100px_auto]">
+                  <span>Brand :</span>
+                  <span>Toyota</span>
+                </div>
+                <div className="grid grid-cols-[100px_auto]">
+                  <span>Model :</span>
+                  <span>Camry</span>
+                </div>
+                <div className="grid grid-cols-[100px_auto]">
+                  <span>Menu. Year :</span>
+                  <span>2019</span>
+                </div>
+                <div className="grid grid-cols-[100px_auto]">
+                  <span>Reg. Plant :</span>
+                  <span>ABC123</span>
+                </div>
+              </div>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 };
--- src/pages/afterLogin/userManagement/myBookings/UpcomingBooking.tsx
+++ src/pages/afterLogin/userManagement/myBookings/UpcomingBooking.tsx
@@ -6,7 +6,37 @@ const UpcomingBooking = () => {
       <div className="flex items-center justify-between flex-col sm:flex-row gap-2 mb-10">
         <h1 className="text-lg sm:text-xl font-bold">Upcoming Bookings</h1>
       </div>
-      
+      <section className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-5">
+        <div className="shadow-md rounded-lg">
+          <div className="h-28 bg-red-300 text-gray-900 text-xl font-bold flex justify-center items-center rounded-tl-md rounded-tr-md">
+            <span>00:00:00</span>
+          </div>
+          <div className="p-4 text-center sm:text-start">
+            <p className="text-base font-semibold mb-1">Car Disinfecting</p>
+            <p className="text-sm font-semibold mb-2">$50</p>
+            <p className="text-sm flex items-center justify-center sm:justify-start gap-1 mb-1">
+              <span>
+                <FaCalendarAlt />
+              </span>
+              <span>2024-07-31</span>
+            </p>
+            <div className="text-sm flex flex-col items-center sm:items-start gap-1 min-w-20">
+              <div className="flex items-center gap-1">
+                <span className="flex items-center gap-1">
+                  <FaRegClock /> Start :
+                </span>
+                <span>12:00</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <span className="flex items-center gap-1">
+                  <FaRegClock /> End :
+                </span>
+                <span>13:00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
